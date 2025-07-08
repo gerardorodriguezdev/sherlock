25db3612d6a797d38944db691a5b3197cc3566cc
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-07-08 17:44:09 +0100

    Makes it case agnostic

--- composeApp/src/commonMain/kotlin/org/sherlock/processor/TextExtractor.kt
+++ composeApp/src/commonMain/kotlin/org/sherlock/processor/TextExtractor.kt
@@ -35,7 +35,7 @@ class TextExtractor(
     }
 
     private fun String.toTokens(): Set<String> =
-        regex.findAll(this).map { matchResult -> matchResult.value }.toSet()
+        regex.findAll(this).map { matchResult -> matchResult.value.lowercase() }.toSet()
 
     suspend fun search(text: String): List<String> =
         withContext(dispatchersProvider.io()) {
