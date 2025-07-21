0ec894fb8dc3291777e9a08695e4a70bcf67a1da
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-07-21 22:32:48 +0100

    Adds cocoapods plugin to gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kmp)
+    alias(libs.plugins.nativeCocoapods)
     alias(libs.plugins.androidLibrary)
 }
 
@@ -8,6 +9,24 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Kotlin sample project with CocoaPods dependencies"
+        homepage = "https://developers.google.com/ml-kit/vision/text-recognition/v2/ios#objc"
+        version = "1.16.2"
+
+        ios.deploymentTarget = "16.0"
+
+        pod("GoogleMLKit/Vision") {
+            moduleName = "MLKitVision"
+            version = "9.0.0"
+        }
+
+        pod("GoogleMLKit/TextRecognitionCommon") {
+            moduleName = "MLKitTextRecognitionCommon"
+            version = "9.0.0"
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
